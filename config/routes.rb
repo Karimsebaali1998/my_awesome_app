62b72cef54d374144bf662d40568e10f1f4d40e2
Karim Al Sebaali <karimalsebaali@gmail.com>
2021-03-02 07:27:23 -0800

    Finished my awesome app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/login', to: 'login#index'
+  get '/', to: 'login#index'
 
 end
